df98353ada718c558e1db0f762123f5b686e9fdc
cao.anqi <cao.anqi@synyi.com>
2021-08-03 15:13:37 +0800

    fix bug

--- tag.go
+++ tag.go
@@ -7,7 +7,6 @@ import (
 	"github.com/loki-os/go-ethernet-ip/messages/packet"
 	"github.com/loki-os/go-ethernet-ip/path"
 	"github.com/loki-os/go-ethernet-ip/types"
-	"sync"
 )
 
 const (
@@ -43,8 +42,7 @@ var TypeMap = map[types.UInt]string{
 }
 
 type Tag struct {
-	Lock *sync.Mutex
-	TCP  *EIPTCP
+	TCP *EIPTCP
 
 	instanceID types.UDInt
 	nameLen    types.UInt
@@ -61,8 +59,6 @@ type Tag struct {
 }
 
 func (t *Tag) Read() error {
-	t.Lock.Lock()
-	defer t.Lock.Unlock()
 	res, err := t.TCP.Send(t.readRequest())
 	if err != nil {
 		return err
@@ -111,16 +107,19 @@ func (t *Tag) readParser(mr *packet.MessageRouterResponse, cb func(func())) {
 }
 
 func (t *Tag) Write() error {
-	t.Lock.Lock()
-	defer t.Lock.Unlock()
-	_, err := t.TCP.Send(multiple(t.writeRequest()))
-	if err == nil {
-		if t.wValue != nil {
+	if t.changed {
+		_, err := t.TCP.Send(multiple(t.writeRequest()))
+		if err == nil {
 			copy(t.value, t.wValue)
 			t.wValue = nil
+			if t.Onchange != nil {
+				t.Onchange()
+			}
 		}
+		t.changed = false
+		return err
 	}
-	return err
+	return nil
 }
 
 func (t *Tag) writeRequest() []*packet.MessageRouterRequest {
@@ -312,7 +311,6 @@ func (t *EIPTCP) allTags(tagMap map[string]*Tag, instanceID types.UDInt) (map[st
 	for io1.Len() > 0 {
 		tag := new(Tag)
 		tag.TCP = t
-		tag.Lock = new(sync.Mutex)
 
 		io1.RL(&tag.instanceID)
 		io1.RL(&tag.nameLen)
@@ -373,16 +371,9 @@ func (tg *TagGroup) Read() error {
 	var mrs []*packet.MessageRouterRequest
 
 	for i := range tg.tags {
-		tg.tags[i].Lock.Lock()
 		list = append(list, tg.tags[i].instanceID)
 		mrs = append(mrs, tg.tags[i].readRequest())
 	}
-
-	defer func() {
-		for i := range tg.tags {
-			tg.tags[i].Lock.Unlock()
-		}
-	}()
 
 	_sb := multiple(mrs)
 	res, err := tg.Tcp.Send(_sb)
@@ -431,19 +422,18 @@ func (tg *TagGroup) Write() error {
 	var mrs []*packet.MessageRouterRequest
 
 	for i := range tg.tags {
-		tg.tags[i].Lock.Lock()
 		if tg.tags[i].changed {
+			defer func() {
+				copy(tg.tags[i].value, tg.tags[i].wValue)
+				if tg.tags[i].Onchange != nil {
+					tg.tags[i].Onchange()
+				}
+			}()
 			list = append(list, tg.tags[i].instanceID)
 			mrs = append(mrs, tg.tags[i].writeRequest()...)
 			tg.tags[i].changed = false
 		}
 	}
-
-	defer func() {
-		for i := range tg.tags {
-			tg.tags[i].Lock.Unlock()
-		}
-	}()
 
 	if len(list) == 0 {
 		return nil
@@ -452,12 +442,6 @@ func (tg *TagGroup) Write() error {
 	_, err := tg.Tcp.Send(multiple(mrs))
 	if err != nil {
 		return err
-	}
-	for i := range tg.tags {
-		if tg.tags[i].wValue != nil {
-			copy(tg.tags[i].value, tg.tags[i].wValue)
-			tg.tags[i].wValue = nil
-		}
 	}
 
 	return nil
